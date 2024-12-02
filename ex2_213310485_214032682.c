0f96c64ba89a9349238c68a827471b2b9b4955da
saleh2411 <salehkhalil1124@gmail.com>
2024-12-02 22:11:48 +0200

    ex2

--- ex2_213310485_214032682.c
+++ ex2_213310485_214032682.c
@@ -5,6 +5,28 @@
 // SECTION A: Recursive Subsequences Generation
 void generate_subsequences(char* str, char* current, int index) {
     // TODO: Implement the recursive logic for generating subsequences
+    
+    // Base case: If we've reached the end of the string
+    if (str[index] == '\0') {
+        printf("\"%s\"\n", current); // Print the current subsequence
+        return;
+    }
+
+    // Exclude the current character and move to the next
+    generate_subsequences(str, current, index + 1);
+
+    // Include the current character and move to the next
+    int length = strlen(current);    // Current length of the 'current' string
+    current[length] = str[index];   // Add the current character
+    current[length + 1] = '\0';     // Null-terminate the string
+
+
+    generate_subsequences(str, current, index + 1);
+
+    // Backtrack: Remove the last added character
+    current[length] = '\0';
+
+    
 }
 
 // Driver Program
